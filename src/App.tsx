908021ce6081f7b706e462825f8d573ff8b46639
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2024-01-12 17:58:05 +0300

    instal redux toolkit and create store

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Auth from "./pages/Auth/Auth";
 
 const App = () => {
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { store } from "./store/store.ts";
+import { Provider } from "react-redux";
 import "./styles/_reset.scss";
 import "./styles/styles.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
