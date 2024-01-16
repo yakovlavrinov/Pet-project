4e017a0ffe82ec4f18df12301ca6dfd41d42735f
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2024-01-16 20:26:48 +0300

    adding variables to scss and editing styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { store } from "./redux/store/store.ts";
 import { Provider } from "react-redux";
-import "./styles/_reset.scss";
 import "./styles/styles.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
